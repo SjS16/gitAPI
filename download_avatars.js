b6979aa67be7c909eab5f012798959860f94ddc7
Stephanie Simpson <stephsimpson911@gmail.com>
2018-01-22 22:28:34 +0000

    added token to authorize

--- download_avatars.js
+++ download_avatars.js
@@ -1,2 +1,24 @@
 var request = require('request');
+var gitHubToken = require('./secret');
+
 console.log("Welcome to the Github Avatar Downloader!");
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+  url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+  headers: {
+    'User-Agent': 'request',
+    'Authorization': 'gitHubToken'
+  }
+};
+  request(options, function(err, res, body) {
+    cb(err, body);
+
+  });
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
