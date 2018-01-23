42e9de2e603bcf7d0aff80b5d1d62453bc848b1d
Stephanie Simpson <stephsimpson911@gmail.com>
2018-01-23 01:19:33 +0000

    added download image by url with hardcoded strings

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var gitHubToken = require('./secret');
+var fs = require('fs');
 
 console.log("Welcome to the Github Avatar Downloader!");
 
@@ -26,3 +27,16 @@ getRepoContributors("jquery", "jquery", function(err, users) {
   })
   //console.log("Result:", result.length, result);
 });
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
