b321c7af0602f25cc23513e270fa5a7f0d7ee2c1
Stephanie Simpson <stephsimpson911@gmail.com>
2018-01-23 03:45:44 +0000

    made both parameters in node required

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,9 @@
+var args = process.argv;
 var request = require('request');
 var gitHubToken = require('./secret');
 var fs = require('fs');
 
 console.log("Welcome to the Github Avatar Downloader!");
-console.log(gitHubToken.GITHUB_TOKEN)
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -17,14 +17,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
   })
 }
 
-getRepoContributors("jquery", "jquery", function(err, users) {
-  console.log("Errors:", err);
-  users.forEach(function(user) {
+getRepoContributors(args[2], args[3], function(err, users) {
+  if (args.length < 4)
+   {console.log("Specify both arguments!");
+ return;
+}
+   users.forEach(function(user) {
     var url = user.avatar_url;
     var filePath = "avatars/" + user.login + ".jpg";
     console.log(downloadImageByURL(url, filePath));
-  })
-
+    });
 });
 
 function downloadImageByURL(url, filePath) {
