9c3a632ce264ff71f37698fe0526c6dd483e11a9
Stephanie Simpson <stephsimpson911@gmail.com>
2018-01-23 04:14:51 +0000

    added comments

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,14 @@
+// get arguments form node
 var args = process.argv;
 var request = require('request');
+//require token from secret gitignore space
 var gitHubToken = require('./secret');
 var fs = require('fs');
 
+//start er up
 console.log("Welcome to the Github Avatar Downloader!");
+
+//function to get the information from url and parse it into array
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -17,36 +22,35 @@ function getRepoContributors(repoOwner, repoName, cb) {
   })
 }
 
+//use args from node to fill in and get repo contributors
 getRepoContributors(args[2], args[3], function(err, users) {
-  if (args.length < 4)
+  if (args.length < 4) //require both args
    {console.log("Specify both arguments!");
  return;
 }
-  fs.mkdir('./avatars', function(err, fd) {
+  fs.mkdir('./avatars', function(err, fd) { //make avatar directory
       if (err) {
         return console.error(err);
       }
         console.log("Directory opened successfully!");
   });
-   users.forEach(function(user) {
+   users.forEach(function(user) { //go through each user in array to get needed parameters
     var url = user.avatar_url;
     var filePath = "avatars/" + user.login + ".jpg";
-    console.log(downloadImageByURL(url, filePath));
+    console.log(downloadImageByURL(url, filePath)); //use parsed info from tags in download image function
   });
 
 });
 
 
 function downloadImageByURL(url, filePath) {
-  request.get(url)               // Note 1
-       .on('error', function (err) {                                   // Note 2
+  request.get(url)               // use request to find the url
+       .on('error', function (err) {
          throw err;
        })
-       .on('response', function (response) {                           // Note 3
-         console.log('Response Status Code: ', response.statusCode);
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode); //use to make sure they come through correctly
        })
-       .pipe(fs.createWriteStream(filePath));
+       .pipe(fs.createWriteStream(filePath)); //pull in images to file path made in getrepocontributor function
        return;
 }
-
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
